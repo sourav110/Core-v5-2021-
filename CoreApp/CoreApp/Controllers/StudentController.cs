85f291095a2cef83d105d75793c4e60a7d113202
Sourav110 <rdfox.007@gmail.com>
2021-04-03 02:43:47 +0600

    jq-ui added

---
+++ CoreApp/CoreApp/Controllers/StudentController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly AppDbContext _context;
+        public StudentController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddStudent()
+        {
+            var student = new Student();
+            return View(student);
+        }
+
+        [HttpPost]
+        public IActionResult AddStudent(Student student, IFormFile StdPhoto)
+        {
+            //if (StdPhoto != null)
+            //{
+            //    using (var ms = new MemoryStream())
+            //    {
+
+            //        StdPhoto.CopyTo(ms);
+            //        student.StudentPhoto = ms.ToArray();
+            //    }
+            //}
+
+            student.StudentPhoto = GetByteArrayFromImage(StdPhoto);
+            _context.Add(student);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        private byte[] GetByteArrayFromImage(IFormFile file)
+        {
+            using (var target = new MemoryStream())
+            {
+                file.CopyTo(target);
+                return target.ToArray();
+            }
+        }
+    }
+}
---
+++ CoreApp/CoreApp/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public byte[] StudentPhoto { get; set; }
+    }
+}
